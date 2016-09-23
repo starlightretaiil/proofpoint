61332cdc962943bd7c969f0de2ee90b73d347b73
michael earls <michael@michaelearls.com>
2016-09-23 15:35:32 -0400

    Added domain for organisations
    Added get domain(s) for organisations(s)

--- main.py
+++ main.py
@@ -43,3 +43,31 @@ def list_user_email(username, password, domain, email):
     r = requests.get(url, headers=headers)
     get_json = r.json()
     print(get_json)
+
+
+def list_orgs_domain(username, password, domain):
+    '''
+    :param username:
+    :param password:
+    :param domain:
+    :return: Returns all domains identified by :domain belonging to the organisation in JSON format
+    '''
+    url = 'https://us1.proofpointessentials.com/api/domain/' + domain + '/'
+    headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
+    r = requests.get(url, headers=headers)
+    get_json = r.json()
+    print(get_json)
+
+
+def list_org_domain(username, password, domain):
+    '''
+    :param username:
+    :param password:
+    :param domain:
+    :return: Returns a single domain :domain in JSON format
+    '''
+    url = 'https://us1.proofpointessentials.com/api/domain/' + domain + '/'  + domain
+    headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
+    r = requests.get(url, headers=headers)
+    get_json = r.json()
+    print(get_json)
