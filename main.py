9cee6b5ff8a78bdc29b2858a206c324561f73dca
Michael Earls <michael@michaelearls.com>
2019-08-17 16:17:37 -0400

    Update main.py
    This script supports Proofpoint API V1.0

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import requests
-import json
 
-
-def list_orgs_info(username, password, domain):
-    '''
-    :param username:
-    :param password:
-    :param domain:
-    :return: Return all organisations under a parent organisation identified by :domain in JSON format
-    '''
-    url = 'https://us1.proofpointessentials.com/api/orgs/' + domain + '/' + 'orgs'
-    headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
-    r = requests.get(url, headers=headers)
-    get_json = r.json()
-    print(get_json)
-
+'''
+This script pulls data out of Poofpoint API and returns the data as json 
+Proofpoint API v1.0
+Michael Earls
+www.michaelearls.com
+Python 3.7
+'''
 
 def list_org_info(username, password, domain):
     '''
@@ -23,7 +15,7 @@ def list_org_info(username, password, domain):
     :param domain:
     :return: Returns a single organisation identified by :domain in JSON format
     '''
-    url = 'https://us1.proofpointessentials.com/api/orgs/' + domain
+    url = 'https://us1.proofpointessentials.com/api/v1/orgs/' + domain
     headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers)
     get_json = r.json()
@@ -38,21 +30,21 @@ def list_user_email(username, password, domain, email):
     :param email:
     :return: Returns a single user identified by :email from the organisation identified by :domain in JSON format
     '''
-    url = 'https://us1.proofpointessentials.com/api/orgs/' + domain + '/' + 'users' + '/' + email
+    url = 'https://us1.proofpointessentials.com/api/v1/orgs/' + domain + '/' + 'users' + '/' + email
     headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers)
     get_json = r.json()
     print(get_json)
 
 
-def list_orgs_domain(username, password, domain):
+def list_org_licensing(username, password, domain):
     '''
     :param username:
     :param password:
     :param domain:
-    :return: Returns all domains identified by :domain belonging to the organisation in JSON format
+    :return: Returns organisation licensing information
     '''
-    url = 'https://us1.proofpointessentials.com/api/domain/' + domain + '/'
+    url = 'https://us1.proofpointessentials.com/api/v1/orgs/' + domain + '/' + 'licensing'
     headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers)
     get_json = r.json()
@@ -66,8 +58,24 @@ def list_org_domain(username, password, domain):
     :param domain:
     :return: Returns a single domain :domain in JSON format
     '''
-    url = 'https://us1.proofpointessentials.com/api/domain/' + domain + '/'  + domain
+    url = 'https://us1.proofpointessentials.com/api/v1/orgs/' + domain + '/domains/'  + domain
     headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers)
     get_json = r.json()
     print(get_json)
+
+
+def find_endpoint(username, password, domain):
+    '''
+
+    :param username:
+    :param password:
+    :param domain:
+    :return: Returns the Organization URL
+    '''
+    url = 'https://us1.proofpointessentials.com/api/v1/endpoints/' + domain
+    headers = {'X-User': username, 'X-Password': password, 'Content-Type': 'application/json'}
+    r = requests.get(url, headers=headers)
+    get_json = r.json()
+    print(get_json)
+
